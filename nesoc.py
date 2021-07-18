e0c28b0b215fadfda161c612fd8188fc47bc0598
Rahul <rahul.iragavarapu@gmail.com>
2021-07-19 00:39:04 +0530

    Update nesoc.py

--- nesoc.py
+++ nesoc.py
@@ -10,7 +10,7 @@ base_url = "https://www.nseindia.com/"
 index_url = "https://www.nseindia.com/api/option-chain-indices?symbol={}"
 equity_url = "https://www.nseindia.com/api/option-chain-equities?symbol={}"
 
-root_dir=""
+root_dir = ""
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     'accept-encoding': 'gzip, deflate, br'
@@ -21,7 +21,7 @@ try:
     cookies = dict(request.cookies)
     for index in indices:
         url = index_url.format(index)
-        resp = requests.Session().get(url, headers=headers, timeout=10, cookies=cookies )
+        resp = requests.Session().get(url, headers=headers, timeout=10, cookies=cookies)
         data = resp.json()
         data.pop('filtered')
         data = data['records']
@@ -30,10 +30,21 @@ try:
         ref_time = data['timestamp']
         d = datetime.strptime(ref_time, "%d-%b-%Y %H:%M:%S")
         d = d.strftime("%d%m%Y_%H%M%S")
-        with open(os.path.join(path,d+'.json'),'w') as f:
-            json.dump(data,f)
+        with open(os.path.join(path, d + '_' + index + '.json'), 'w') as f:
+            json.dump(data, f)
+
+    for equity in equities:
+        url = equity_url.format(equity)
+        resp = requests.Session().get(url, headers=headers, timeout=10, cookies=cookies)
+        data = resp.json()
+        data.pop('filtered')
+        data = data['records']
+        path = os.path.join(root_dir, str(datetime.now().date()))
+        os.makedirs(path, exist_ok=True)
+        ref_time = data['timestamp']
+        d = datetime.strptime(ref_time, "%d-%b-%Y %H:%M:%S")
+        d = d.strftime("%d%m%Y_%H%M%S")
+        with open(os.path.join(path, d + '_' + equity + '.json'), 'w') as f:
+            json.dump(data, f)
 except:
     traceback.print_exc()
-    
-#crontab
-#* 10-16 * * * python nseoc.py
